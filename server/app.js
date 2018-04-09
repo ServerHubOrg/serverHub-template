33106a8eb29153839680462d47a702398ccceaec
Yang Zhongdong <yangzd1996@outlook.com>
2018-04-09 08:54:35 +0800

    Update: fit ServerHub MVC v1.0.3

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@
  * Demo App Entry
  * 
  * ServerHub CLI, MIT License
- * ServerHub MVC version 0.0.93
+ * ServerHub MVC version 1.0.3
  * Yang Zhongdong (yangzd1996@outlook.com)
  */
 const serverhub = require('serverhub-mvc');
--- server/controller/home.shc.js
+++ server/controller/home.shc.js
@@ -1,13 +1,15 @@
 "use strict";
-
-return {
+/**
+ * This file is composed with module style controller syntax, which is avaliabe since ServerHub v1.0.3
+ */
+module.exports = {
     index: function (req, res, method) {
         this.Console.log('Hello, I\'m from Home controller and index action');
         return this.View();
     },
     primary: function (req, res, method) {
         var context = this.View();
-        context.name = 'Runtime.version -> ' + this.Runtime.branch;
+        context.name = 'System.version -> ' + this.System.Version;
         return context;
     }
 };
